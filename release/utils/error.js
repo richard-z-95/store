6a56b56b4f8b9c9a53d89c74eade5211509414cd
zhaochenwei <chenwei.zhao@wuage.com>
2018-05-16 14:27:27 +0800

    init project

---
+++ release/utils/error.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var errorMapping = exports.errorMapping = {
+  LOCALSTOREAGE_NOT_SUPPORTED: 'localStorage not supported',
+  INVALID_PARAMETERS: 'invalid parameters',
+  UPDATE_TYPE_MISMATCH: 'parameters type mismatch',
+  KEY_NOT_EXISTED: 'key does not existed'
+};
