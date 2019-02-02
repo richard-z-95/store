72d66a70631fb0968de1ecb2f1650a0c4d94b1ba
IRONGit <anseszcw@gmail.com>
2019-02-02 16:57:57 +0800

    chore: update config

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,16 @@ module.exports = {
         umdNamedDefine: true,
         libraryExport: 'default'
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
     mode: 'production'
 }
